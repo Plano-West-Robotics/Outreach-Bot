fbcd135b31ca912c79846842bbc518131aadc5d5
Aryan Bangad <aryanparagbangad@gmail.com>
2023-09-21 19:40:50 -0500

    Added PurplePixelDetection.java, added PpdTrial.java
    Woohoo Fourth commit!!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/PpdTrial.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.*;
+
+public class PpdTrial  {
+    private PurplePixelDetection pipe;
+    private OpenCvWebcam web;
+    private Telemetry telemetry;
+
+    public PpdTrial(LinearOpMode op)
+    {
+        web = OpenCvCameraFactory.getInstance().createWebcam(
+                op.hardwareMap.get(WebcamName.class,"webcam")
+        );
+        telemetry = op.telemetry;
+    }
+
+    public void findStuff()
+    {
+        pipe = new PurplePixelDetection(telemetry);
+
+        web.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                web.setPipeline(pipe);
+                web.startStreaming(680, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Error:", "Camera produced error code: " + errorCode);
+                stop();
+            }
+        });
+    }
+
+    public void stop()
+    {
+        web.stopStreaming();
+    }
+}
